14669afe5a62cee9997dfc309f75b490f02765ae
wnnafisah <23nafisahjunaidi@ktj.edu.my>
2022-03-11 16:39:58 +0800

    Todoo App

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:todoapp/widgets.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(
+            horizontal: 24.0,
+          ),
+          color: Color(0xFFF6F6F6),
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(
+                      top: 32.0,
+                      bottom: 32.0,
+                    ),
+                    child: Image(
+                      image: AssetImage(
+                        'assets/images/logo.png'
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ListView(
+                      children: [
+                        TaskCardWidget(
+                          title: 'Get Started!',
+                          desc: 'Hello User! Welcome to TODOO App :D',
+                        ),
+                        TaskCardWidget(title: '(Unnamed Task)', desc: 'No Description Added',),
+                        TaskCardWidget(title: '(Unnamed Task)', desc: 'No Description Added',),
+                        TaskCardWidget(title: '(Unnamed Task)', desc: 'No Description Added',),
+                        TaskCardWidget(title: '(Unnamed Task)', desc: 'No Description Added',),
+                      ]
+                    ),
+                  ),
+                ],
+              ),
+              Positioned(
+                bottom: 24.0,
+                right: 0.0,
+                child: Container(
+                  width: 60.0,
+                  height: 60.0,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF7349FE),
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Image(
+                    image: AssetImage(
+                      "assets/images/add_icon.png"
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
